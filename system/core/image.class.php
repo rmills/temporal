8d9f446528075bb9e54def31a93b9e1a46e16252
Ryan Mills <ryan@ryanmills.net>
2012-11-26 16:22:11 -0800

    add docs to image.class.php

--- system/core/image.class.php
+++ system/core/image.class.php
@@ -17,20 +17,49 @@ if (!defined('IMAGE_DEFAULT_SQUARE')) {
 }
 
 class Image {
-
+    /**
+     * @var string filename 
+     */
     private $_file = false;
+    
+    /**
+     *
+     * @var string name used for title
+     */
     private $_name = false;
+    
+    /**
+     *
+     * @var array all data for an image
+     */
     private $_data = array();
+    
+    /**
+     * @var int id of Image() 
+     */
     private $_id = 0;
+    
+    /**
+     * @var bool load error check
+     */
     public $_error = false;
-
+    
+    /**
+     * Try to load an image
+     * @param int $id
+     */
     public function __construct($id = false) {
         $this->check_folders();
         if (is_numeric($id)) {
             $this->load($id);
         }
     }
-
+    
+    /**
+     * Add new image to system
+     * @param string $file
+     * @param string $name
+     */
     public function create($file, $name = false) {
         $orginal = IMAGE_ORGINAL_PATH . $file;
         $ext = pathinfo($orginal);
@@ -46,7 +75,12 @@ class Image {
             )';
         \DB::q($sql);
     }
-
+    
+    /**
+     * Load based on id or filename
+     * @param mixed $image_id_or_file
+     * @return boolean
+     */
     public function load($image_id_or_file) {
         if (is_numeric($image_id_or_file)) {
             $sql = 'SELECT * FROM `images` WHERE `id` = \'' . \DB::clean($image_id_or_file) . '\' LIMIT 1';
@@ -67,7 +101,10 @@ class Image {
         $this->_error = 'Image not found';
         return false;
     }
-
+    
+    /**
+     * Make sure all folders for cache are created
+     */
     public function check_folders() {
 
         if (!is_dir(CACHE_PATH)) {
@@ -84,19 +121,36 @@ class Image {
             mkdir(IMAGE_ORGINAL_PATH);
         }
     }
-
-    public function name($name = false) {
+    
+    /**
+     * Returns name/title of image
+     * @param str $name
+     * @return type
+     */
+    public function name() {
         return $this->_name;
     }
-
+    
+    /**
+     * Place Holder, not used
+     */
     public function setname() {
         
     }
-
+    
+    /**
+     * Place Holder, not used
+     */
     public function delete() {
         
     }
-
+    
+    /**
+     * Returns html tag for thumbnail
+     * @param int $size
+     * @param bool $square is square
+     * @return html <img> tag
+     */
     public function thumbnail($size = IMAGE_DEFAULT_THUMBNAIL_SIZE, $square = IMAGE_DEFAULT_SQUARE) {
         $this->check_cache($this->_file, $size, $square);
         $path = PATH_BASE.IMAGE_CACHE_PATH . $square . '/' . $size . '/';
@@ -105,18 +159,40 @@ class Image {
         }
         return '<img src="' . $path . $this->_file . '" title="' . $this->_name . '" alt="' . $this->_name . '">';
     }
-
+    
+    /**
+     * Wraps thumbnail() to create a link
+     * @param string $href path for link
+     * @param int $size thumbnail height
+     * @param bool $square thumbnail is square
+     * @param string $class class atrib for <a> tag
+     * @return html
+     */
     public function thumbnail_link($href, $size = IMAGE_DEFAULT_THUMBNAIL_SIZE, $square = IMAGE_DEFAULT_SQUARE, $class = false) {
         $this->check_cache($this->_file, $size, $square);
         return '<a class="'.$class.'" href="' . $href . '">' . $this->thumbnail($size, $square) . '</a>';
     }
     
+    /**
+     * Display thumbnail for new image size, like a thumbnail for a larger image
+     * @param int $display_size height of image loaded
+     * @param int $size height of thumbnail
+     * @param type $square thumbnail is square
+     * @param type $class class for <a> tag on thumbnail
+     * @return type
+     */
     public function thumbnail_display_link($display_size, $size = IMAGE_DEFAULT_THUMBNAIL_SIZE, $square = IMAGE_DEFAULT_SQUARE, $class = false) {
         $this->check_cache($this->_file, $display_size, 0);
         $href = PATH_BASE.IMAGE_CACHE_PATH .'0'.'/' . $display_size . '/'.$this->_file;
         return '<a class="'.$class.'" href="' . $href . '">' . $this->thumbnail($size, $square) . '</a>';
     }
-
+    
+    /**
+     * Check if image has cached resize created
+     * @param string $file filename
+     * @param int $size height of image
+     * @param int $square image is square
+     */
     private function check_cache($file, $size, $square) {
         $path1 = IMAGE_CACHE_PATH . $square . '/';
         $path2 = $path1 . $size . '/';
@@ -133,7 +209,14 @@ class Image {
             $this->create_cache_file($size, $square, $path2, $file);
         }
     }
-
+    
+    /**
+     * Create a cache version of Image
+     * @param int $size height of cache image
+     * @param type $square new image is square
+     * @param string $path to image (not used, legacy)
+     * @param string $file filename
+     */
     private function create_cache_file($size, $square, $path, $file) {
         $path = IMAGE_CACHE_PATH . $square . '/' . $size . '/';
         copy(IMAGE_ORGINAL_PATH . $file, $path . $file);
@@ -143,7 +226,13 @@ class Image {
             Image::resize($path . $file, $size);
         }
     }
-
+    
+    /**
+     * Create a url safe filename
+     * @param string $ext file ext
+     * @param int $len len of filename
+     * @return string
+     */
     public static function random_filename($ext, $len = 10) {
         $result = "";
         $charPool = '0123456789abcdefghijklmnopqrstuvwxyz';
@@ -159,7 +248,14 @@ class Image {
         }
         return $result;
     }
-
+    
+    /**
+     * Resize an image
+     * @param string $imagepath path to image
+     * @param int $size resize
+     * @param bool $resize_by_height force resize by height, recomended
+     * @return boolean
+     */
     public static function resize($imagepath, $size, $resize_by_height = true) {
 
         if (!is_file($imagepath)) {
