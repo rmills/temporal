ad209f338014e19cd52ad5c217e005880f71ef52
Ryan Mills <ryan@ryanmills.net>
2013-08-05 12:08:04 -0700

    add ifmodule for cache in .htaccess, move growl to footer

--- system/modules/js_jgrowl/Js_jgrowl.mod.php
+++ system/modules/js_jgrowl/Js_jgrowl.mod.php
@@ -5,12 +5,12 @@ namespace Module;
 class Js_jgrowl extends Module {
 
     public static function __registar_callback() {
-        \CMS::callstack_add('add_assets', DEFAULT_CALLBACK_CREATE);
+        \CMS::callstack_add('parse', DEFAULT_CALLBACK_CREATE);
     }
 
     public static function add_assets() {
-        \Html::$_css[] = '<link href="' . PATH_BASE . 'system/modules/js_jgrowl/assets/jquery.jgrowl.css" type="text/css" rel="stylesheet">';
-        \Html::$_scripts[] = '<script type="text/javascript" src="' . PATH_BASE . 'system/modules/js_jgrowl/assets/jquery.jgrowl.js"></script>';
+        \Html::set('{footer}', '<link href="' . PATH_BASE . 'system/modules/js_jgrowl/assets/jquery.jgrowl.css" type="text/css" rel="stylesheet">');
+        \Html::set('{footer}', '<script type="text/javascript" src="' . PATH_BASE . 'system/modules/js_jgrowl/assets/jquery.jgrowl.js"></script>');
     }
 
 }
