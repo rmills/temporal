443cf70701d7c96f25f059a990b26f2bd42856c6
Ryan Mills <ryan@ryanmills.net>
2013-09-11 22:26:12 -0700

    fix site debug

--- system/core/sitedebug.class.php
+++ system/core/sitedebug.class.php
@@ -7,15 +7,25 @@ class SiteDebug{
             return false;
         }
         
-        $bug_details = array();
-        $bug_details['stacktrace1'] = 'Line: '.\DB::clean($trace[0]['line']).' - '.\DB::clean($trace[0]['file']);
-        $bug_details['stacktrace2'] = 'Line: '.\DB::clean($trace[1]['line']).' - '.\DB::clean($trace[1]['file']);
-        $bug_details['stacktrace3'] = 'Line: '.\DB::clean($trace[2]['line']).' - '.\DB::clean($trace[2]['file']);
-        $bug_details['stacktrace4'] = 'Line: '.\DB::clean($trace[3]['line']).' - '.\DB::clean($trace[3]['file']);
-        $bug_details['msg'] = \DB::clean($msg);
-        $bug_details['time'] = time();
-        
-        $bug = new AutoDB('site_debug');
-        $bug->init($bug_details);
+        if(is_array($trace)){
+            $bug_details = array();
+            if(isset($trace[0]['line'])){
+                $bug_details['stacktrace1'] = 'Line: '.\DB::clean($trace[0]['line']).' - '.\DB::clean($trace[0]['file']);
+            }
+            if(isset($trace[1]['line'])){
+                $bug_details['stacktrace1'] = 'Line: '.\DB::clean($trace[1]['line']).' - '.\DB::clean($trace[1]['file']);
+            }
+            if(isset($trace[2]['line'])){
+                $bug_details['stacktrace1'] = 'Line: '.\DB::clean($trace[2]['line']).' - '.\DB::clean($trace[2]['file']);
+            }
+            if(isset($trace[3]['line'])){
+                $bug_details['stacktrace1'] = 'Line: '.\DB::clean($trace[3]['line']).' - '.\DB::clean($trace[3]['file']);
+            }
+            $bug_details['msg'] = \DB::clean($msg);
+            $bug_details['time'] = time();
+
+            $bug = new AutoDB('site_debug');
+            $bug->init($bug_details);
+        }
     }
 }
