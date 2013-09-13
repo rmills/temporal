ba11c92953383ee823dc29b0a2c16a97210858b1
Ryan Mills <ryan@ryanmills.net>
2013-09-13 02:11:55 -0700

    add cache key

--- system/modules/js_jgrowl/Js_jgrowl.mod.php
+++ system/modules/js_jgrowl/Js_jgrowl.mod.php
@@ -8,9 +8,9 @@ class Js_jgrowl extends Module {
         \CMS::callstack_add('parse', DEFAULT_CALLBACK_CREATE);
     }
 
-    public static function add_assets() {
-        \Html::set('{footer}', '<link href="' . PATH_BASE . 'system/modules/js_jgrowl/assets/jquery.jgrowl.css" type="text/css" rel="stylesheet">');
-        \Html::set('{footer}', '<script type="text/javascript" src="' . PATH_BASE . 'system/modules/js_jgrowl/assets/jquery.jgrowl.js"></script>');
+    public static function parse() {
+        \Html::set('{css}', '<link href="' . PATH_BASE . 'system/modules/js_jgrowl/assets/jquery.jgrowl.css" type="text/css" rel="stylesheet">');
+        \Html::set('{scripts}', '<script type="text/javascript" src="' . PATH_BASE . 'system/modules/js_jgrowl/assets/jquery.jgrowl.js"></script>');
     }
 
 }
