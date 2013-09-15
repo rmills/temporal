3320742992c5b2d763fc288f40ac54b31388cca6
Ryan Mills <ryan@ryanmills.net>
2013-09-14 18:21:55 -0700

    hide admin bar on mobile

--- system/modules/admin_bar/admin_bar.mod.php
+++ system/modules/admin_bar/admin_bar.mod.php
@@ -13,16 +13,19 @@ class Admin_bar extends Module {
     }
 
     public static function set_tags() {
-        $html = self::build_admin_bar();
-        ksort(\Page\Admin::$_quicklinks);
-        $i = 0;
-        foreach (\Page\Admin::$_quicklinks as $v) {
-            $break = '<li class="divider-vertical"></li>';
-            $html = str_replace('{adminlinks}', $v . $break . '{adminlinks}', $html);
-            $i++;
+        if($mobile_check = new \Mobile_Detect();
+            if(!$mobile_check->isMobile()){)
+            $html = self::build_admin_bar();
+            ksort(\Page\Admin::$_quicklinks);
+            $i = 0;
+            foreach (\Page\Admin::$_quicklinks as $v) {
+                $break = '<li class="divider-vertical"></li>';
+                $html = str_replace('{adminlinks}', $v . $break . '{adminlinks}', $html);
+                $i++;
+            }
+            $html = str_replace('{adminlinks}', '', $html);
+            \Html::set('{adminbar}', $html);
         }
-        $html = str_replace('{adminlinks}', '', $html);
-        \Html::set('{adminbar}', $html);
     }
 
     public static function build_admin_bar() {
